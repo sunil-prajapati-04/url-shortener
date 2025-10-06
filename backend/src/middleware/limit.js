62fe4878b670ffd5a2fffa055be7484235cfd978
Sunil Prajapati <2023cssunil14308@poornima.edu.in>
2025-10-06 19:56:36 +0530

    reslove limiter file

--- backend/src/middleware/limit.js
+++ backend/src/middleware/limit.js
@@ -1,4 +1,4 @@
-import {rateLimit} from 'express-rate-limit';
+import {rateLimit,ipKeyGenerator} from 'express-rate-limit';
 
 let now = new Date();
 let windowMs = 12*60*60*1000;
@@ -6,7 +6,7 @@ const limiter = rateLimit({
     windowMs:windowMs, //12hours
     limit:(req,res)=> (req.user?0:3),
     //keyGenerator isliye use huva kyuki ratelimit be default har Ip pe track karta hain limit ko mtlb multiple user same ip address aaye toh uske liye same limit lagegi but agar user login hain toh uske user.id pe rateLimit Track karega aur har user ki id different he hoti hain 
-    keyGenerator:(req,res) =>( req.user? req.user.id:req.ip), 
+    keyGenerator:(req,res) =>( req.user? req.user.id:ipKeyGenerator(req, res)), 
     handler:(req,res)=>{
         res.status(429).json({
             success:false,
