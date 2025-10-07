e29531a39acf3a93eace7db8ea672ad65dbac192
Sunil Prajapati <2023cssunil14308@poornima.edu.in>
2025-10-07 20:43:49 +0530

    upadate frontend serve route

--- backend/src/index.js
+++ backend/src/index.js
@@ -59,7 +59,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
   
 
-  app.get("/*All", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
